0d7967cf9cae0b2f71c7f16120135ad0a2ff08ef
Anthony Russo <anthony.russo@watttime.org>
2025-07-29 17:39:30 -0600

    updated percentiles file including cement and oil and gas methodology updates

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ CONFIG = {
     "asset_path": "data/asset_emissions_country_subsector.parquet",
     "country_subsector_stats_path": "data/country_subsector_emissions_statistics_202507.parquet",
     "country_subsector_totals_path": "data/country_subsector_emissions_totals_202507.parquet",
-    "percentile_path": "data/static/ct_percentile_40sectors_moer_stat_industrial_20250619.parquet",
+    "percentile_path": "data/static/ct_percentile_40sectors_moer_stat_industrial_20250729.parquet",
     "annual_asset_path": "data/asset_annual_emissions/chunk_*.parquet",
     "city_path": "data/emissions_reduction/city_emissions.parquet",
     "gadm_1_path": "data/emissions_reduction/gadm_1_emissions.parquet",
